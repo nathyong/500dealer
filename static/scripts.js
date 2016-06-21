342f943962b4178afdcb9e5172cc2d32c903eba4
Vince Au <vinceau09@gmail.com>
2016-06-22 01:23:09 +0900

    Show the cards as pictures

---
+++ static/scripts.js
@@ -0,0 +1,36 @@
+function getSuit(suit) {
+    if (suit === 'S') {
+        return 'spades';
+    }
+    else if (suit === 'C') {
+        return 'clubs';
+    }
+    else if (suit === 'D') {
+        return 'diamonds';
+    }
+    else if (suit === 'H') {
+        return 'hearts';
+    }
+    return 'joker';
+}
+
+function getRank(value) {
+    //joker is 0 but show 1 anyway
+    if (value === 0 || value === 14) {
+        return 1;
+    }
+    else if (value > 10) {
+        return value + 3;
+    }
+    return value;
+}
+
+function generateCard(card) {
+    var item = document.createElement("div");
+    var classes = ['card'];
+    classes.push(getSuit(card.suit));
+    classes.push('rank' + getRank(card.value));
+    item.className = classes.join(' ');
+    item.innerHTML = '<div class="face"></div';
+    return item;
+}
